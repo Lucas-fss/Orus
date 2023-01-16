4c3cfd249e441f6872f9f354c9c71977d44debf2
imsouza <mateusalmeida071@icloud.com>
2023-01-16 14:35:55 -0300

    update css

--- app/models/connection.js
+++ app/models/connection.js
@@ -4,7 +4,7 @@ class Connection{
         this.connection = this.mysql.createConnection({
             host     : 'localhost',
             user     : 'root',
-            password : '33218600Mm#', 
+            password : '123456', 
             database : 'Orus'
         });
     }
